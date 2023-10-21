651eafe7e80b2bb0ac6d169aa8f9489d08aa20a9
Nepu <bepis.interprises2021@gmail.com>
2023-10-21 02:55:44 -0300

    introduction to math object

---
+++ JavaScript/6-math/index.js
@@ -0,0 +1,19 @@
+let x = 3.14;
+let y = 5;
+let z = 9;
+let maximum;
+let minimum;
+
+//x = Math.round(x); //rounds number
+//x = Math.floor(x); //rounds number DOWN
+//x = Math.ceil(x); //rounds number UP
+//x = Math.pow(x, 3); //exponent
+//x = Math.sqrt(x); //square root
+//x = Math.abs(x); //absolute (turns negative to positive)
+
+//maximum = Math.max(x, y, z); //bigger number
+//minimum = Math.min(x, y, z); //smaller number
+
+x = Math.PI //ratio of the circumference of a circle
+
+document.getElementById("displayValue").innerHTML = x;
---
+++ JavaScript/6-math/readme.md
@@ -0,0 +1,7 @@
+**Math**
+An intrinsic object that provides basic mathematics functionality
+and constants.
+
+In JavaScript, intrinsic objects, also known as built-in objects,
+are a set of pre-defined objects that are always available in the
+JavaScript environment, regardless of the context or platform.
