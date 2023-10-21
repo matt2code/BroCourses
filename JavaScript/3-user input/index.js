05efee7c11230dd250604681d4e7907c47c0cb08
Nepu <bepis.interprises2021@gmail.com>
2023-10-21 01:28:25 -0300

    nepu!

---
+++ JavaScript/3-user input/index.js
@@ -0,0 +1,12 @@
+//window prompt (easy way)
+//let username = window.prompt("Provide a name:");
+//console.log(username);
+
+//html textbox (difficult way, but more practical)
+let username;
+document.getElementById("confirmBtn").onclick = function(){ //acesses the document, fires a function after clicking on the element with the "confirmBtn" id.
+    username = document.getElementById("namePrompt").value; //assigns the value inside the element with the "namePrompt" id.
+    console.log(username); //displays the assigned username
+
+    document.getElementById("labelHello").innerHTML = "Hello " + username; //changes the INNER content of the element with the "labelHello" id.
+}
---
+++ JavaScript/3-user input/readme.md
@@ -0,0 +1,4 @@
+How to accept user input
+
+- **EASY WAY**: window prompt
+- **DIFFUCULT WAY** (but more practical): HTML textbox
