c0dd1f9fb2a3c868002943c8730d640ca3ab65c2
Nepu <bepis.interprises2021@gmail.com>
2023-10-21 00:41:00 -0300

    subscribe to become a fellow bro

---
+++ JavaScript/arithmetic expressions/index.js
@@ -0,0 +1,22 @@
+let students = 20;
+let extraStudents
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+//extraStudents = students % 2;
+
+//augmented assignment operator, basically a shortcut
+//students += 1;
+//students -= 1;
+//students *= 2;
+//students /= 2;
+
+
+let result = 1 + 2**2 * (3 + 4) / (3 - 1);
+
+
+//console.log(students);
+//console.log(extraStudents);
+console.log(result);
---
+++ JavaScript/arithmetic expressions/readme.md
@@ -0,0 +1,14 @@
+**Arithmetic** expressions is a combination of:
+- operands (values, variables, etc.)
+- operators (+ - * / %)
+
+That can be evaluated to a value.
+ex.: y = x + 5;
+
+Also contains operator precedence:
+1. paranthesis ()
+2. exponents **
+3. multiplication *
+4. division /
+5. addition +
+6. subtraction -
